e7ef436c832965fb5fd7a734ea124ae5204c73db
Ruben Vereecken <rubenvereecken@gmail.com>
2014-05-13 10:55:37 +0200

    Refactored some. Added rule for checking amount of boats

---
+++ grammar.pl
@@ -0,0 +1,53 @@
+%Input grammar
+boat(n).
+boat(e).
+boat(s).
+boat(w).
+boat(x).
+water('~').
+
+%%north
+north(n, '~').
+north(o, '~').
+north(e, '~').
+north(s, n).
+north(s, x).
+north(w, '~').
+north(x, _).
+north('~', _).
+
+%%East
+east(n, '~').
+east(o, '~').
+east(e, '~').
+east(s, '~').
+east(w, x).
+east(w, e).
+east(x, _).
+east('~', _).
+
+%%South
+south(n, x).
+south(n, s).
+south(o, '~').
+south(e, '~').
+south(s, '~').
+south(w, '~').
+south(x, _).
+south('~', _).
+
+%%West
+west(n, '~').
+west(o, '~').
+west(e, x).
+west(e, w).
+west(s, '~').
+west(w, '~').
+west(x, _).
+west('~', _).
+
+%%diagonal
+diagonal(_, '~').
+diagonal('~', _).
+
+
--- rules.pl
+++ rules.pl
@@ -10,48 +10,11 @@ getXYElement(X, Y, [_|Tail], E) :- Y1 is Y-1, getXYElement(X, Y1, Tail, E).
 width([Head|_], X) :- length(Head, X). %width
 height(Field, X) :- length(Field, X). %height
 
-%%north
-north(n, '~').
-north(o, '~').
-north(e, '~').
-north(s, n).
-north(s, x).
-north(w, '~').
-north(x, _).
-north('~', _).
-
-%%East
-east(n, '~').
-east(o, '~').
-east(e, '~').
-east(s, '~').
-east(w, x).
-east(w, e).
-east(x, _).
-east('~', _).
-
-%%South
-south(n, x).
-south(n, s).
-south(o, '~').
-south(e, '~').
-south(s, '~').
-south(w, '~').
-south(x, _).
-south('~', _).
-
-%%West
-west(n, '~').
-west(o, '~').
-west(e, x).
-west(e, w).
-west(s, '~').
-west(w, '~').
-west(x, _).
-west('~', _).
-
-%%diagonal
-diagonal(_, '~').
-diagonal('~', _).
+%Check whether field satisfies the numbers on the side
+checkFieldStrip([], 0).
+checkFieldStrip([StripHead|StripTail], BoatCount) :- 
+    boat(StripHead), NextCount is BoatCount - 1, checkFieldStrip(StripTail, NextCount);
+    water(StripHead), checkFieldStrip(StripTail, BoatCount).
+   
 
 checkAround(X, Y, Field) :- rows(Field, Rows), columns(Field, Columns), X > 0, Y > 0, X < Rows, Y < Columns, XS is X-1, YS is Y-1, XA is X+1, YA is Y+1, getXYElement(X, Y, Field, E), getXYElement(XS, YS, Field, LeftUp), getXYElement(XS, Y, Field, Left), getXYElement(XS, YA, Field, LeftDown), getXYElement(X, YS, Field, Up), getXYElement(X, YA, Field, Down), getXYElement(XA, YS, Field, RightUp), getXYElement(XA, Y, Field, Right), getXYElement(XA, YA, Field, RightDown), north(E, Up), east(E, Left), south(E, Down), west(E, Right), diagonal(E, LeftUp), diagonal(E, RightUp), diagonal(E, LeftDown), diagonal(E, RightDown).
